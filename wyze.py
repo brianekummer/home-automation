195be1c59fe59fee2a138b9e4ebe3ca8230b8aa6
Brian Kummer <Brian-Kummer@teletracking.com>
2021-06-03 16:06:23 -0400

    Refactored parsing and validation to handle action_value

--- wyze.py
+++ wyze.py
@@ -1,19 +1,43 @@
 """
-  Home automation scripts for Wyze devices
+  Home automation script for Wyze devices
   by Brian Kummer, June 2021
+  Uses this unofficial API: https://pypi.org/project/wyze-sdk/
 
-  This script expects the following environmental variables to be set
-    WYZE_EMAIL="xxxxx@gmail.com"
-    WYZE_PASSWORD="xxxxxxxx"
-    WYZE_DEVICE_FAN="plug|xxxxxxxxxxxx"
-    WYZE_DEVICE_AC="plug|xxxxxxxxxxxx"
+  Syntax:
+    wyze.py <device-name> <action> <action-value>
 
-  TO DO
-    * Why is this so slow? Is much time taken by auth? If so, cache the
-      var client, like this: 
-        https://datascience.blog.wzb.eu/2016/08/12/a-tip-for-the-impatient-simple-caching-with-python-pickle-and-decorators/
-        https://able.bio/dfernsby/pickling-objects-with-python--0342z5b
+    wyze.py <plug-name>   on|off
+
+    wyze.py <bulb-name>   on|off
+                          bright   <brightness>
+                          temp     <color-temperature>
+
+    Examples:
+      wyze.py fan on
+      wyze.py ac off
+      wyze.py desklite on
+      wyze.py desklite bright 100
+      wyze.py desklite bright +
+      wyze.py desklite temp 3800
+
+  Assumptions:
+    * This script expects the following environmental variables to be set
+        - For authentication, which requires 2FA on my Wyze account to be
+          disabled
+            WYZE_EMAIL="xxxxx@gmail.com"
+            WYZE_PASSWORD="xxxxxxxx"
+        - Each device needs to have an environmental variable (set in 
+          ~/.env) with its type and MAC address, like these:
+            WYZE_DEVICE_FAN="plug|ABABABABABAB"
+            WYZE_DEVICE_AC="plug|121212121212"
+
+  Notes:
+    * Authenticating every time I call this script is unnecessarily slow,
+      so I cache the authenticated client and read it from disk instead of
+      regenerating it every time I run this script.
 """
+
+
 import sys
 import os
 import pickle
@@ -21,9 +45,7 @@ from wyze_sdk import Client
 from wyze_sdk.errors import WyzeApiError
 from os import path
 
-#import os.path
-WYZE_CLIENT_FILENAME = "wyze_client.txt"
-
+WYZE_CLIENT_FILENAME = 'wyze_client.txt'
 
 
 """
@@ -39,95 +61,119 @@ def create_wyze_client():
     new_file = open(WYZE_CLIENT_FILENAME, 'rb')
     client = pickle.load(new_file)
   else:
-    # create the file
     client = Client(email=os.environ.get('WYZE_EMAIL'), password=os.environ.get('WYZE_PASSWORD'))
     file = open(WYZE_CLIENT_FILENAME, 'wb')
     pickle.dump(client, file)
     file.close()
-
   return client
 
 
 """
-  Validate a device name and parse the device's type and MAC address from
-  its environmental variable
+  Parse the script's parameters into a device_type, device_mac, action,
+  and action_value. The device's name is used to find the environment
+  variable that contains the device's type and MAC address.
 
   Inputs:
-    * The device's name (i.e. "fan")
-
-  Outputs:
-    * An error message if the device name is invalid
+    * Parameters passed into the script
+    * Environment variables named WYZE_DEVICE_xxxx that define
+      the device type and MAC address for the device
 
   Returns:
-    * Is the device's name valid?
-    * The device's type
+    * The device's type (plug|bulb)
     * The device's MAC address
+    * The action
+    * The action_value (brightness, color temperature)
 """
-def validate_and_parse_device(device_name): 
-  device_env_variable = f"WYZE_DEVICE_{device_name.upper()}"
-  device_info = os.environ.get(device_env_variable)
-  if device_info:
-    device_parts = device_info.split("|")
-    return True, device_parts[0], device_parts[1]
+def parse_parameters(params):
+  device_name = params[1].lower() if len(params) > 1 else None
+  action = params[2].lower() if len(params) > 2 else None
+  action_value = params[3].lower() if len(params) > 3 else None
+
+  if device_name:
+    device_env_variable = f"WYZE_DEVICE_{device_name.upper()}"
+    device_info = os.environ.get(device_env_variable)
+    if device_info:
+      device_parts = device_info.split('|')
+      return device_parts[0], device_parts[1], action, action_value
+    else:
+      print(f"Device {device_name} isn't defined- set environment variable {device_env_variable}\n")
   else:
-    print(f"Device {device_name} doesn't exist- set environment variable {device_env_variable}")
-    return False, "", ""
+    print(f"device-name is a required field\n")
+
+  return None, None, None, None
 
 
 """
-  Validate the action
+  Validate the action value
 
   Inputs:
-    * The action name (i.e. "on")
-
-  Outputs:
-    * An error message if the action is invalid
+    * Action value type (None|brightness|color-remperature)
+    * Action value to validate
 
   Returns:
-    * Is the action valid?
+    * Is it valid?
 """
-def validate_action(action):
-  if action != "on" and action != "off":
-    print(f"Invalid action")
-    return False
+def validate_action_value(action_value_type, action_value):
+  is_valid = False
+
+  if action_value_type == None:
+    is_valid = True
+
+  elif action_value_type == "brightness":
+    if action_value == None:
+      print(f"action-value is a required field\n")
+    elif (action_value.isnumeric() and 0 <= action_value <= 100) or action_value in {'+', '-'}:
+      is_valid = True
+    else:
+      print(f"{action_value} is not a valid brightness value\n")
+
+  elif action_value_type == "color-temperature":
+    if action_value == None:
+      print(f"action-value is a required field\n")
+    elif action_value.isnumeric() or action_value in {'+', '-'}:
+      is_valid = True
+    else:
+      print(f"{action_value} is not a valid color temperature value\n")
+      
   else:
-    return True
+    print(f"{action_value_type} is an invalid action value type\n")
+  
+  return is_valid
 
 
 """
-  Validate and parse the command-line parameters
+  Validate the parameters. Ensures that we don't try to set the brightness
+  on a smart plug.
 
   Inputs:
-    * The device's name (i.e. "fan")
-    * The action to perform on that device (i.e. "on")
-
-  Outputs:
-    * Syntax help if 2 parameters are not passed
-    * Error message(s) if the device name and/or action are invalid
-
-  Returns:
-    * Are all the parameters valid?
     * The device's type
-    * The device's MAC address
-    * The action to perform on the device
+    * The action
+    * The action's value
 """
-def validate_and_parse_parameters(params):
-  device_type = device_mac = action = ""
-  is_valid_device_name = is_valid_action = False
+def validate_parameters(device_type, action, action_value):
+  validations = {
+    'plug': { 'on':     None,
+              'off':    None }, 
+    'bulb': { 'on':     None,
+              'off':    None,
+              'bright': 'brightness',
+              'temp':   'color-temperature' }
+  }
 
-  if len(params) != 3:
-    print(f"Syntax:")
-    print(f"    wyze.py <device_name> <action>")
-    print(f"        device_name: fan|ac")
-    print(f"        action:      on|off")
-  else:
-    device_name = params[1].lower()
-    action = params[2].lower()
+  is_valid = False
+  if device_type in validations:
+    if action in validations[device_type]:
+      action_value_type = validations[device_type][action]
+      is_valid = validate_action_value(action_value_type, action_value)
+    else:
+      if action == None:
+        print('action is a required field\n')
+      else:
+        print(f"{action} is not a valid action\n")
+  elif device_type != None:
+    print(f"{device_type} is not a valid device type\n")
 
-    is_valid_device_name, device_type, device_mac = validate_and_parse_device(device_name)
-    is_valid_action = validate_action(action)
-
-  return is_valid_device_name and is_valid_action, device_type, device_mac, action
+  return is_valid
 
 
 """
@@ -142,9 +188,9 @@ def validate_and_parse_parameters(params):
 """
 def plug_action(device_mac, action):
   plug = client.plugs.info(device_mac=device_mac)
-  if action == "off":
+  if action == 'off':
     client.plugs.turn_off(device_mac=plug.mac, device_model=plug.product.model)
-  else:
+  elif action == 'on':
     client.plugs.turn_on(device_mac=plug.mac, device_model=plug.product.model)
 
 
@@ -154,29 +200,58 @@ def plug_action(device_mac, action):
   Inputs:
     * The device's MAC address
     * The action to perform on the device
+    * The value for the action (brightness or color temperature)
 
   Outputs:
     * Turns the bulb on or off
+      OR
+    * Sets the bulb's brightness
+      OR
+    * Sets the bulb's color temperature
 """
-def bulb_action():
-  #TODO: duh!
-  pass
+def bulb_action(device_mac, action, action_value):
+  bulb = client.bulbs.info(device_mac=device_mac)
+  if action == 'off':
+    client.bulbs.turn_off(device_mac=bulb.mac, device_model=bulb.product.model)
+  elif action == 'on':
+    client.bulbs.turn_on(device_mac=bulb.mac, device_model=bulb.product.model)
+  elif action == 'bright':
+    client.bulbs.set_brightness(device_mac=bulb.mac, device_model=bulb.product.model, brightness=action_value)
+  elif action == 'temp':
+    client.bulbs.set_color_temp(device_mac=bulb.mac, device_model=bulb.product.model, color_temp=action_value)
 
 
 """
-  Main body
+  Main body 
 """
 try:
-  client = create_wyze_client()
   is_valid = False
-  device_type = device_mac = action = ""
-  is_valid, device_type, device_mac, action = validate_and_parse_parameters(sys.argv)
-  if is_valid:
-    device_actions = {
-      "plug": plug_action,
-      "bulb": bulb_action
-    }
-    device_actions[device_type](device_mac, action)
+  device_type, device_mac, action, action_value = parse_parameters(sys.argv)
+  is_valid = validate_parameters(device_type, action, action_value)
+  if not is_valid:
+    print('Syntax:')
+    print('  wyze.py <device_name> <action> <action-value>')
+    print('    device_name:  fan|ac|desklite')
+    print('    action:       on|off|bright|temp')
+    print('    action-value: brightness or color temperature ')
+    print('')
+    print('  For plugs, valid actions are on|off')
+    print('  For bulbs, valid actions are on|off|bright|temp')
+    print('    actions of bright|temp require action_value, is either a number or +/-')
+    print('')
+    print('Examples:')
+    print('  wyze.py fan on')
+    print('  wyze.py ac off')
+    print('  wyze.py desklite on')
+    print('  wyze.py desklite bright 100')
+    print('  wyze.py desklite bright +')
+    print('  wyze.py desklite temp 3800')
+  else:
+    client = create_wyze_client()
+    if device_type == 'plug':
+      plug_action(device_mac, action)
+    elif device_type == 'bulb':
+      bulb_action(device_mac, action, action_value)
 
 except WyzeApiError as e:
     # You will get a WyzeApiError if the request failed
